dee9000aca7012d2de61176c753555edabf6a137
GreatGoat123 <forarjan123@gmail.com>
2017-01-29 21:48:39 -0500

    Add files via upload
    Java classes used for the EntryScreenGUI and the algorithim

---
+++ EntryScreen.java
@@ -0,0 +1,13 @@
+package trids.trids_alpha_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_screen);
+    }
+}
---
+++ MathClass.java
@@ -0,0 +1,8 @@
+package trids.trids_alpha_v1;
+
+/**
+ * Created by Arjan on 1/29/2017.
+ */
+
+public class MathClass {
+}
